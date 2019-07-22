82a970c9a4eb8b5911662b0b608c4bc9af9f558e
tbmoon <taebong.moon87@gmail.com>
2019-07-22 11:59:02 -0400

    first commit

---
+++ data_loader.py
@@ -0,0 +1,63 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as data
+
+
+class YouTubeDataset(data.Dataset):
+
+    def __init__(self, input_dir, phase, max_frame_length=301):
+        self.input_dir = input_dir + 'npy_formatted_frame/validate/'
+        self.df = pd.read_csv(input_dir + phase + '.csv')
+        self.max_frame_length = max_frame_length
+
+    def __getitem__(self, idx):
+        data = np.load(self.input_dir + self.df['id'][idx], allow_pickle=True).item()
+        segment_labels = data['segment_labels']
+        segment_scores = data['segment_scores']
+        segment_start_times = data['segment_start_times']
+        frame_rgb = data['frame_rgb']
+        frame_audio = data['frame_audio']
+        rgb_feature_size = len(frame_rgb[0])      # rgb_feature_size: 1024
+        audio_feature_size = len(frame_audio[0])  # audio_feature_size: 128
+        
+        padded_frame_rgb = np.array([np.array([0.] * rgb_feature_size)] * self.max_frame_length)
+        padded_frame_rgb[:len(frame_rgb)] = frame_rgb
+        padded_frame_audio = np.array([np.array([0.] * audio_feature_size)] * self.max_frame_length)
+        padded_frame_audio[:len(frame_audio)] = frame_audio
+
+        sample = {
+            'segment_labels': segment_labels,
+            'segment_scores': segment_scores,
+            'segment_start_times': segment_start_times,
+            'frame_rgb': padded_frame_rgb,
+            'frame_audio': padded_frame_audio}
+        return sample
+
+    def __len__(self):
+        return len(self.df)
+
+
+def get_dataloader(
+    input_dir,
+    max_frame_length,
+    batch_size,
+    num_workers):
+
+    youtube_dataset = {
+        phase: YouTubeDataset(
+            input_dir=input_dir,
+            phase=phase,
+            max_frame_length=max_frame_length)
+        for phase in ['train', 'valid']}
+
+    data_loader = {
+        phase: torch.utils.data.DataLoader(
+            dataset=youtube_dataset[phase],
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers)
+        for phase in ['train', 'valid']}
+
+    return data_loader
