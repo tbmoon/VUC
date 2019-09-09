145b136272891c96cce3345e25df8eecacef2572
tbmoon <taebong.moon87@gmail.com>
2019-09-09 15:29:22 -0400

    new loss

---
+++ center_loss.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+
+class CenterLoss(nn.Module):
+    """Center loss.
+    
+    Reference:
+    Wen et al. A Discriminative Feature Learning Approach for Deep Face Recognition. ECCV 2016.
+    
+    Args:
+        num_classes (int): number of classes.
+        feat_dim (int): feature dimension.
+    """
+    def __init__(self, num_classes, feat_dim, device):
+        super(CenterLoss, self).__init__()
+        self.num_classes = num_classes
+        self.feat_dim = feat_dim
+        self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).to(device))
+
+    def forward(self, x, labels, device):
+        """
+        Args:
+            x: feature matrix with shape (batch_size, feat_dim).
+            labels: ground truth labels with shape (batch_size).
+        """
+        batch_size = x.size(0)
+        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+                  torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
+        distmat.addmm_(1, -2, x, self.centers.t())
+
+        zero_class = torch.LongTensor([-9999]).to(device)
+        non_zero_class = torch.arange(1, self.num_classes).long().to(device)
+        classes = torch.cat((zero_class, non_zero_class), dim=0)
+
+        labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
+        mask = labels.eq(classes.expand(batch_size, self.num_classes))
+
+        dist = distmat * mask.float()
+        loss = dist.clamp(min=1e-12, max=1e+12).sum()
+
+        return loss
